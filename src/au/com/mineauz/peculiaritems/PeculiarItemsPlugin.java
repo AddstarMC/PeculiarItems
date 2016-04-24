c026dda874b47c6f48a2130286edb25f272781b0
Schmoller <steven.schmoll@gmail.com>
2016-04-25 00:26:36 +1000

    Fixed some loading issues

--- src/au/com/mineauz/peculiaritems/PeculiarItemsPlugin.java
+++ src/au/com/mineauz/peculiaritems/PeculiarItemsPlugin.java
@@ -1,7 +1,6 @@
 package au.com.mineauz.peculiaritems;
 
-import java.io.File;
-
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import au.com.mineauz.peculiaritems.commands.CommandDispatcher;
@@ -9,42 +8,22 @@ import au.com.mineauz.peculiaritems.peculiarstats.PeculiarStat;
 import au.com.mineauz.peculiaritems.peculiarstats.PeculiarStats;
 import au.com.mineauz.peculiaritems.peculiarstats.StatListener;
 
-public class PeculiarItemsPlugin extends JavaPlugin{
-	
+public class PeculiarItemsPlugin extends JavaPlugin {
 	private boolean broadcastRank = true;
 	private static PeculiarItemsPlugin plugin;
 	private Data data;
 	private PeculiarStats stats;
 	
 	@Override
-	public void onEnable(){
-		
+	public void onEnable() {
 		plugin = this;
 		
-		File conf = new File(getDataFolder() + "/" + "config.yml");
-		if(!conf.exists()){
-			saveResource("config.yml", false);
-		}
-		try{
-			getConfig().load(getDataFolder() + "/config.yml");
-		}catch(Exception e){
-			getLogger().severe("Unable to load config.yml!");
-			e.printStackTrace();
-		}
+		saveDefaultConfig();
+		reloadConfig();
 		
 		data = new Data();
 		
-		for(String rank : getConfig().getConfigurationSection("ranks").getKeys(false)){
-			if(!rank.matches("[0-9]+")){
-				data.clearRanks();
-				for(String rank2 : getConfig().getConfigurationSection("ranks").getKeys(false)){
-					data.setRank(Integer.parseInt(rank2), getConfig().getString("ranks." + rank2));
-				}
-				break;
-			}
-			else
-				data.setRank(Integer.parseInt(rank), getConfig().getString("ranks." + rank));
-		}
+		loadRanks();
 		broadcastRank = getConfig().getBoolean("broadcastRankIncrease");
 
 		CommandDispatcher comd = new CommandDispatcher();
@@ -56,6 +35,23 @@ public class PeculiarItemsPlugin extends JavaPlugin{
 		getServer().getPluginManager().registerEvents(new Events(), this);
 		getServer().getPluginManager().registerEvents(new StatListener(), this);
 		getLogger().info("Peculiar Items successfully enabled!");
+	}
+	
+	private void loadRanks() {
+		ConfigurationSection rankSection = getConfig().getConfigurationSection("ranks");
+		if (rankSection == null) {
+			return;
+		}
+		
+		data.clearRanks();
+		for (String rank : rankSection.getKeys(false)) {
+			try {
+				int value = Integer.parseInt(rank);
+				data.setRank(value, rankSection.getString(rank));
+			} catch (NumberFormatException e) {
+				getLogger().warning("Invalid rank definition: " + rank);
+			}
+		}
 	}
 	
 	@Override
